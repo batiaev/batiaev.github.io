8762cb95f8f7c062425a24bd40558d5e1356502b
Anton Batiaev <a.batyaev@gmail.com>
2021-09-05 10:16:36 +0100

    update dependencies

---
+++ docs/_next/static/Koc2tdS7_6R2kK0Tki5I9/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/20-41596643827fd40eec02.js","static/chunks/pages/index-65116be47c3e992b55b3.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
