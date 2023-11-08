842f1fdc4152f71b79ce92f34b8b5f50ddd8b0cd
Anton Batiaev <anton@batiaev.com>
2023-11-08 22:45:55 +0000

    add anchor contact me

--- src/scenes/Contact.tsx
+++ src/scenes/Contact.tsx
@@ -3,7 +3,7 @@ import {InlineWidget} from "react-calendly";
 
 export function Contact() {
     return (
-        <section>
+        <section id={"contactMe"}>
             <Title text="Get In Touch"/>
             <InlineWidget url="https://calendly.com/batiaev/30min"/>
         </section>
